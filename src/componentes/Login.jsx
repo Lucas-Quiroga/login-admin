4c29f33eada3045a4f9f2a1345d72b21a87eea92
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-09-13 15:39:20 -0300

    login

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "./Menu";
 
 export default function Login() {
+  const [miLogin, setMiLogin] = useState("false");
+  const [usu, setUsu] = useState("");
+  const [pas, setPas] = useState("");
+
+  //   armamos una funcion para evaluar los datos
+  const iniciarSesion = (e) => {
+    //la función evita el recargo de pagina
+    //agarra los IDs de los inputs junto con su valor
+    //evalua con un IF si los valores de los IDs están en cero y arroja una alerta
+    //despues evalua si los states guardan los datos ya hardcodeados y transforma el estado en TRUE
+    //y sino lo deja en FALSE y limpia los inputs
+    e.preventDefault();
+    let txtusu = document.getElementById("txtusu").value;
+    let txtpas = document.getElementById("txtpas").value;
+    if (txtusu.length === 0 || txtpas.length === 0) {
+      alert("complete los datos faltantes");
+    } else {
+      if (usu === "admin" && pas === "123") {
+        setMiLogin("true");
+        document.getElementById("form_login").style.display = "none";
+      } else {
+        setMiLogin("false");
+        alert("error de usuario y/o contraseña!");
+        document.getElementById("txtusu").value = "";
+        document.getElementById("txtpas").value = "";
+        document.getElementById("txtusu").focus();
+      }
+    }
+  };
+
   return (
     <div
       className="container"
@@ -14,9 +45,12 @@ export default function Login() {
           </label>
           <input
             type="text"
+            //id del input
             id="txtusu"
             style={{ textAlign: "center" }}
             className="form-control"
+            // capturamos el dato
+            onChange={(e) => setUsu(e.target.value)}
             required
           />
         </div>
@@ -26,20 +60,26 @@ export default function Login() {
           </label>
           <input
             type="password"
+            //id del input
             id="txtpas"
             style={{ textAlign: "center" }}
             className="form-control"
+            // capturamos el dato
+            onChange={(e) => setPas(e.target.value)}
             required
           />
         </div>
         <br />
         <input
           type="submit"
-          //   onClick={iniciarSesion}
+          onClick={iniciarSesion}
           className="btn btn-primary"
           value="Login"
         />
       </form>
+
+      {/* condición que muestra el menu si el estado se transforma en true */}
+      {miLogin === "true" && <Menu />}
     </div>
   );
 }
